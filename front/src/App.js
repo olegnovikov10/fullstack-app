b481d72850b319cdaa8976569f68aaf03199fb08
relaxin07 <relaxinfh@mail.ru>
2021-12-05 02:24:45 +0200

    feat : add userPost component

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {UserPostContainer} from "./containers/userPost/index";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <UserPostContainer firstName="Oleg" secondName="Novikov" date="14.08.2021"
+                                   content="Hello! It's my new post"/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
