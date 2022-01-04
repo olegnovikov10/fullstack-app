8b9ac58091515a22a8487a1d121a0392b06f7f9a
olegnovikov10 <relaxinfh071@gmail.com>
2022-01-04 16:36:12 +0200

    feat: setting up the environment

---
+++ front/.prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    printWidth: 70,
+    useTabs: true,
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+};
--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import {UserPostContainer} from "./containers/userPost/index";
-
+import logo from './logo.svg'
+import './App.css'
+import { UserPostContainer } from './containers/userPost/index'
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <UserPostContainer firstName="Oleg" secondName="Novikov" date="14.08.2021"
-                                   content="Hello! It's my new post"/>
-            </header>
-        </div>
-    );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<UserPostContainer
+					firstName="Oleg"
+					secondName="Novikov"
+					date="14.08.2021"
+					content="Hello! It's my new post"
+				/>
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
