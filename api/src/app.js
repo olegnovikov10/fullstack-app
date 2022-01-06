69526179f286d3828a81eb440ff1d668631b04a2
olegnovikov10 <relaxinfh071@gmail.com>
2022-01-06 21:04:16 +0200

    feat : add sequelize && pg

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,25 @@
 const express = require('express')
+
+const sequelize = require('./services/db')
+
+const config = require('./services/config')
+
 const app = express()
+
+const start = async () => {
+	try {
+		await sequelize.authenticate()
+		await sequelize.sync()
+		app.listen(config.appPort, () => {
+			console.log('work')
+		})
+	} catch (e) {
+		console.log(e)
+	}
+}
+
+start()
 
 app.get('/', function (req, res) {
 	res.send('Hello World')
 })
-
-app.listen(process.env.APP_PORT)
