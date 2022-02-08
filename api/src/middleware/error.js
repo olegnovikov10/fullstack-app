cd6f7ec43c219ff2f1a2a7891dc896af89411e49
olegnovikov10 <relaxinfh071@gmail.com>
2022-02-08 14:25:59 +0200

    feat: add logs  , and errorHandler

--- api/src/middleware/error.js
+++ api/src/middleware/error.js
@@ -1,5 +1,5 @@
-module.exports = (err, req, res, next) => {
+// eslint-disable-next-line
+module.exports = (err, req, res,next) => {
 	console.error(err.stack)
 	res.status(500).send('Something broke!')
-	next()
 }
